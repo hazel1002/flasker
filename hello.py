71de7c9089f70c2184ff246afe47e648e0043cef
Hazel <mimihuang1002tw@gmail.com>
2022-08-04 14:01:05 +0800

    fixed AMT_child

--- hello.py
+++ hello.py
@@ -1,6 +1,7 @@
 from crypt import methods
 import email
 from email.policy import default
+import imp
 from flask import Flask, flash, render_template, request
 from flask_wtf import FlaskForm
 from importlib_metadata import method_cache
@@ -9,6 +10,7 @@ from wtforms import StringField, SubmitField, EmailField, IntegerField
 from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from flask_migrate import Migrate
 
 
 # Create a Flask Instance
@@ -20,6 +22,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:12345678@localhost
 app.config['SECRET_KEY'] = "my super secret key that is no one is supposed to know" #create secret key
 # Initialize The Database
 db = SQLAlchemy(app)
+migrate = Migrate(app,db)
 
 
 
@@ -35,6 +38,7 @@ class Users(db.Model):
     email = db.Column(db.String(200), nullable=False, unique=True)
     loantype = db.Column(db.String(200), nullable=False)
     loanAMT = db.Column(db.Integer,nullable=False)
+    AMT_child = db.Column(db.String(10))
     date_added = db.Column(db.DateTime, default=datetime.utcnow)
 
     # Create A String
@@ -68,7 +72,29 @@ class UserForm(FlaskForm):
     email = EmailField("Email", validators=[DataRequired()])
     loantype = StringField("loantype", validators=[DataRequired()])
     loanAMT = IntegerField("loanAMT", validators=[DataRequired()])
+    AMT_child = StringField("AMT_child")
     submit = SubmitField("Submit")
+
+
+# Delete Database Records
+@app.route('/delete/<int:id>')
+def delete(id):
+    user_to_delete = Users.query.get_or_404(id)
+    name = None
+    form = UserForm() 
+    try:
+        db.session.delete(user_to_delete)
+        db.session.commit()
+        flash("User Deleted Successfully!")
+        our_users = Users.query.order_by(Users.date_added)
+        return render_template("add_user.html",form=form, name=name, our_users=our_users)
+    except:
+        db.session.rollback()
+        flash("Woops! There was a problem deleting user, try again! ")
+        return render_template("add_user.html",form=form, name=name, our_users=our_users)
+
+
+
 
 # Update Database Record
 @app.route('/update/<int:id>',methods=['GET','POST'])
@@ -84,16 +110,18 @@ def update(id):
         name_to_update.email = request.form['email']
         name_to_update.loantype = request.form['loantype']
         name_to_update.loanAMT = request.form['loanAMT']
+        name_to_update.AMT_child = request.form['AMT_child']
         try:
             db.session.commit()
             flash("User Updated Successfully!")
-            return render_template("update.html", form=form, name_to_update=name_to_update)
+            return render_template("update.html", form=form, name_to_update=name_to_update, id=id)
         except:
+            db.session.rollback()
             flash("Error! Looks like there was a problem...try again!")
-            return render_template("update.html", form=form, name_to_update=name_to_update)
+            return render_template("update.html", form=form, name_to_update=name_to_update, id=id)
     else:
-        return render_template("update.html", form=form, name_to_update=name_to_update)
-
+        return render_template("update.html", form=form, name_to_update=name_to_update, id=id)
+ 
 
 
 
@@ -110,7 +138,7 @@ def add_user():
         user = Users.query.filter_by(email=form.email.data).first()
         if user is None:
             user = Users(username=form.name.data, bankername=form.bankername.data,
-            gender=form.gender.data, userage=form.userage.data, phonenumber=form.phonenumber.data, email=form.email.data, loantype=form.loantype.data, loanAMT=form.loanAMT.data )
+            gender=form.gender.data, userage=form.userage.data, phonenumber=form.phonenumber.data, email=form.email.data, loantype=form.loantype.data, loanAMT=form.loanAMT.data, AMT_child=form.AMT_child.data )
             db.session.add(user)
             db.session.commit()
         name = form.name.data
@@ -118,10 +146,11 @@ def add_user():
         form.bankername.data = ''
         form.gender.data = ''
         form.userage.data = ''
-        form.phonenumber.dat = ''
+        form.phonenumber.data = ''
         form.email.data = ''
         form.loantype.data = ''
         form.loanAMT.data = ''
+        form.AMT_child.data = ''
         flash("User Added Successfully!!")
     our_users = Users.query.order_by(Users.date_added)
     return render_template("add_user.html",form=form, name=name, our_users=our_users)

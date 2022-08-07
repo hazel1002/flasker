32414fdefce774562c9b199f7646261385b5eaff
Hazel <mimihuang1002tw@gmail.com>
2022-08-07 16:28:34 +0800

    delete post

--- hello.py
+++ hello.py
@@ -49,6 +49,25 @@ class PostForm(FlaskForm):
     slug = StringField("Slug", validators=[DataRequired()])
     submit = SubmitField("Submit")
 
+@app.route('/posts/delete/<int:id>')
+def delete_post(id):
+    post_to_delete = Posts.query.get_or_404(id)
+    try:
+        db.session.delete(post_to_delete)
+        db.session.commit()
+        # Return a message
+        flash("Blog Post Has Been Deleted!")
+        # Grab all the posts from database
+        posts = Posts.query.order_by(Posts.date_posted)
+        return render_template("posts.html", posts=posts)
+    except:
+        # Return an error message
+        flash("Whoops! There was a problem deleting post, try again...")
+        # Grab all the posts from database
+        posts = Posts.query.order_by(Posts.date_posted)
+        return render_template("posts.html", posts=posts)
+
+
 @app.route('/posts')
 def posts():
     # Grab all the posts from database
